89e9f9780240fe771340309eba1fdbdb71133340
Dipesh Thapa <dipesh@naphix.com.au>
2022-12-24 18:22:05 +0545

    List of added spells

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,50 @@
-import Link from 'next/link';
+import React, { useState } from "react";
+import Link from "next/link";
 
 export default function Home({ allSpells }) {
+  // Test starts
+  const [items, setItems] = useState([]);
+  const addItem = (addedItem) => {
+    setItems([
+      ...items,
+      {
+        id: items.length,
+        value: addedItem,
+      },
+    ]);
+  };
+
+  // Test ends
+
   return (
     <>
       <main className="container">
+
+      <ul className="awesome-list favourites">
+          <h2>List of added spells</h2>
+          {items.map((item) => (
+            <li key={item.id}>
+              {item.id + ')'} {item.value}
+            </li>
+          ))}
+        </ul>
+        
         <section className="awesome-list">
           <h2>List of spells</h2>
           {allSpells.map((spell) => (
-            <Link href={ spell.index } className="awesome-list-item" key={spell.url}>
-              <span>{spell.name}</span>
-            </Link>
+            <div className="wrapItem" key={spell.url}>
+              <Link
+                href={spell.index}
+                className="awesome-list-item"
+                key={spell.url}
+              >
+                <span>{spell.name}</span>
+              </Link>
+              <button onClick={() => addItem(spell.name)}>+</button>
+            </div>
           ))}
         </section>
+
       </main>
     </>
   );
@@ -19,7 +52,6 @@ export default function Home({ allSpells }) {
 
 export async function getStaticProps() {
   const response = await fetch("https://www.dnd5eapi.co/api/spells");
-
   const data = await response.json();
 
   return {
